2c9f1d25d40e979ae8eb0e7812974a20452b9411
manojkgorle <gorlemanojk@gmail.com>
2024-12-01 10:44:46 +0530

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 static CONSOLE_LOGGER: ConsoleLogger = ConsoleLogger;
 struct ConsoleLogger;
+
 pub mod fields;
-
-pub mod vm;
-
-pub mod univariate_polynomial;
 pub mod multivariate_polynomial;
+pub mod tables;
+pub mod univariate_polynomial;
+pub mod vm;
 
 fn main() {
     println!("Hello, world!");
