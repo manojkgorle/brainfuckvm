9ffedeca2275bb4b0609ed71762df48bffa4c4a4
soumyathakur44 <soumyathakur44@gmail.com>
2024-12-02 02:14:06 +0530

    blrh

--- src/fri/mod.rs
+++ src/fri/mod.rs
@@ -13,11 +13,35 @@ pub struct FriDomain{
 }
 
 impl FriDomain{
-    fn new(&self, offset: FieldElement,omega: FieldElement, length: usize)->Self{
+    pub fn new(offset: FieldElement,omega: FieldElement, length: usize)->Self{
         Self { offset, omega, length }
     }
-    fn call(&self, index: usize)->FieldElement{
+    pub fn call(&self, index: usize)->FieldElement{
         let _x = self.omega.pow(index as u128)*self.offset;
         _x
     }
+    pub fn list(&self)->Vec<FieldElement>{
+        let mut list: Vec<FieldElement>= vec![];
+        for i in 1..self.length{
+        list.push(self.omega.pow(i as u128)*self.offset);
+        }
+        list
+    }
+    pub fn evaluate(&self, polynomial: Polynomial)-> Vec<FieldElement>{
+        let mut result: Vec<FieldElement> = vec![];
+        for i in 1..polynomial.coefficients.len(){
+            result.push(polynomial.evaluate(self.omega.pow(i as u128)));
+        }
+        result
+    }
+    pub fn xevaluate(&self, polynomial: Polynomial)-> Vec<FieldElement>{
+        let polynomial = polynomial.scalar_mul(self.offset);
+        let mut result: Vec<FieldElement> = vec![];
+        for i in 1..polynomial.coefficients.len(){
+            result.push(polynomial.evaluate(self.omega.pow(i as u128)));
+        }
+        result
+    }
+    
+
 }
