532ab68683fc2c0f2acc65bbf234f2d0521ef72d
m-pandey5 <me1222025@iitd.ac.in>
2024-12-05 21:23:39 +0530

    final_removed

--- src/tables/memory.rs
+++ src/tables/memory.rs
@@ -23,7 +23,7 @@ impl Memory {
         let height = roundup_npow2(length);
         let omicron = derive_omicron(generator, order, height);
         let matrix = vec![vec![FieldElement::zero(field); full_width as usize]; height as usize];
-        let table = Table::new(field, base_width, full_width, length, num_randomizers, height, omicron, generator, order, matrix);
+        let table = Table::new(field, base_width, full_width, length,  height, omicron, generator, order, matrix);
         Self { table: table }
     }
 
