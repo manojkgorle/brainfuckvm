3ab48360cd580686beebe08dc348565997b35f7f
manojkgorle <kfghjkl091@gmail.com>
2024-12-06 18:59:14 +0530

    beautify

--- src/tables/processor.rs
+++ src/tables/processor.rs
@@ -271,7 +271,7 @@ impl ProcessorTable {
 
     //define a selector polynomial for a specific instruction.
     //todo for a set of instructions.
-   pub fn selector_polynomial(
+    pub fn selector_polynomial(
         instruction: char, 
         indeterminate: FieldElement, 
         field: Field,
@@ -288,7 +288,7 @@ impl ProcessorTable {
     }
 
     // define a selector polynomial for a valid set
-   pub fn universal_selector(
+    pub fn universal_selector(
         indeterminate: FieldElement, 
         field: Field,
         char:Vec<char>
@@ -309,116 +309,112 @@ impl ProcessorTable {
         deselectors
     }
 
-
-
-
     //boundary constraints for the base coloumns
     //the values of instructionpermutaion ipa and mpa I am taking as 1
-    pub fn generate_AIR(&self,challenges:Vec<FieldElement>)->Vec<Polynomial>{
-            let interpolated = self.table.clone().interpolate_columns(vec![Indices::Cycle as u128, Indices::InstructionPointer as u128, Indices::CurrentInstruction as u128, Indices::NextInstruction as u128, Indices::MemoryPointer as u128, Indices::MemoryValue as u128, Indices::MemoryValueInverse as u128, Indices::InstructionPermutaion as u128, Indices::MemoryPermuation as u128, Indices::InputEvaluation as u128, Indices::OutputEvaluation as u128]);
-            let clk=interpolated[Indices::Cycle as usize].clone();
-            let ip=interpolated[Indices::InstructionPointer as usize].clone();
-            let ci=interpolated[Indices::CurrentInstruction as usize].clone();
-            let ni=interpolated[Indices::NextInstruction as usize].clone();
-            let mp=interpolated[Indices::MemoryPointer as usize].clone();
-            let mv=interpolated[Indices::MemoryValue as usize].clone();
-            let inv_mv=interpolated[Indices::MemoryValueInverse as usize].clone();
-            let ipa=interpolated[Indices::InstructionPermutaion as usize].clone();
-            let mpa=interpolated[Indices::MemoryPermuation as usize].clone();
-            let iea=interpolated[Indices::InputEvaluation as usize].clone();
-            let oea=interpolated[Indices::OutputEvaluation as usize].clone();
-            let next_interpolated = self.table.clone().next_interpolate_columns(vec![Indices::Cycle as u128, Indices::InstructionPointer as u128, Indices::CurrentInstruction as u128, Indices::NextInstruction as u128, Indices::MemoryPointer as u128, Indices::MemoryValue as u128, Indices::MemoryValueInverse as u128, Indices::InstructionPermutaion as u128, Indices::MemoryPermuation as u128, Indices::InputEvaluation as u128, Indices::OutputEvaluation as u128]);
-            let clk_next=next_interpolated[Indices::Cycle as usize].clone();
-            let ip_next=next_interpolated[Indices::InstructionPointer as usize].clone();
-            let ci_next=next_interpolated[Indices::CurrentInstruction as usize].clone();
-            let ni_next=next_interpolated[Indices::NextInstruction as usize].clone();
-            let mp_next=next_interpolated[Indices::MemoryPointer as usize].clone();
-            let mv_next=next_interpolated[Indices::MemoryValue as usize].clone();
-            let inv_mv_next=next_interpolated[Indices::MemoryValueInverse as usize].clone();
-            let ipa_next=next_interpolated[Indices::InstructionPermutaion as usize].clone();
-            let mpa_next=next_interpolated[Indices::MemoryPermuation as usize].clone();
-            let iea_next=next_interpolated[Indices::InputEvaluation as usize].clone();
-            let oea_next=next_interpolated[Indices::OutputEvaluation as usize].clone();
-            let one=Polynomial::new_from_coefficients(vec![FieldElement::one(self.table.field)]);
-            let mut AIR=vec![];
-            //Boundary contsraints :clk=mp=mv=inv=ip=0
-            //iea=oea=1 (we are using 1 instead of any random number)
-            let poly_two=Polynomial::new_from_coefficients(vec![FieldElement::new(2,self.table.field)]);
-            let boundaryAIR=clk.clone()+ip.clone()+mp.clone()+mv.clone()+inv_mv.clone()+ipa.clone()+mpa.clone()+iea.clone()+oea.clone()-poly_two.clone();
-            AIR.push(boundaryAIR);
-            // Transition_i0,
-            // Transition_i1,
-            // Transition_i2,
-            // Transition_i3,
-            // Transition_i4,
-            // Transition_i5,
-            // Transition_i6,
-            // Transition_i7,
-            // Transition_all,
-            let poly_one=Polynomial::new_from_coefficients(vec![FieldElement::one(self.table.field)]);
-            let mv_is_zero=poly_one.clone()-mv.clone()*inv_mv.clone();
-            //(ip⋆−ip−2)⋅mv+(ip⋆−ni)⋅iszero
-            // mp⋆−mp
-            // mv⋆−mv
-            let trasition_i0=(mv.clone()*(ip_next.clone()-ip.clone()-poly_two.clone())+mv_is_zero.clone()*(ip_next.clone()-ni.clone()))+(mp_next.clone()-mp.clone())+(mv_next.clone()-mv.clone());
-            AIR.push(trasition_i0);
-            // (ip⋆−ip−2)⋅iszero+(ip⋆−ni)⋅mv
-            // mp⋆−mp
-            // mv⋆−mv
+    pub fn generate_air(&self,challenges:Vec<FieldElement>)->Vec<Polynomial>{
+        let interpolated = self.table.clone().interpolate_columns(vec![Indices::Cycle as u128, Indices::InstructionPointer as u128, Indices::CurrentInstruction as u128, Indices::NextInstruction as u128, Indices::MemoryPointer as u128, Indices::MemoryValue as u128, Indices::MemoryValueInverse as u128, Indices::InstructionPermutaion as u128, Indices::MemoryPermuation as u128, Indices::InputEvaluation as u128, Indices::OutputEvaluation as u128]);
+        let clk=interpolated[Indices::Cycle as usize].clone();
+        let ip=interpolated[Indices::InstructionPointer as usize].clone();
+        let ci=interpolated[Indices::CurrentInstruction as usize].clone();
+        let ni=interpolated[Indices::NextInstruction as usize].clone();
+        let mp=interpolated[Indices::MemoryPointer as usize].clone();
+        let mv=interpolated[Indices::MemoryValue as usize].clone();
+        let inv_mv=interpolated[Indices::MemoryValueInverse as usize].clone();
+        let ipa=interpolated[Indices::InstructionPermutaion as usize].clone();
+        let mpa=interpolated[Indices::MemoryPermuation as usize].clone();
+        let iea=interpolated[Indices::InputEvaluation as usize].clone();
+        let oea=interpolated[Indices::OutputEvaluation as usize].clone();
+        let next_interpolated = self.table.clone().next_interpolate_columns(vec![Indices::Cycle as u128, Indices::InstructionPointer as u128, Indices::CurrentInstruction as u128, Indices::NextInstruction as u128, Indices::MemoryPointer as u128, Indices::MemoryValue as u128, Indices::MemoryValueInverse as u128, Indices::InstructionPermutaion as u128, Indices::MemoryPermuation as u128, Indices::InputEvaluation as u128, Indices::OutputEvaluation as u128]);
+        let clk_next=next_interpolated[Indices::Cycle as usize].clone();
+        let ip_next=next_interpolated[Indices::InstructionPointer as usize].clone();
+        let ci_next=next_interpolated[Indices::CurrentInstruction as usize].clone();
+        let ni_next=next_interpolated[Indices::NextInstruction as usize].clone();
+        let mp_next=next_interpolated[Indices::MemoryPointer as usize].clone();
+        let mv_next=next_interpolated[Indices::MemoryValue as usize].clone();
+        let inv_mv_next=next_interpolated[Indices::MemoryValueInverse as usize].clone();
+        let ipa_next=next_interpolated[Indices::InstructionPermutaion as usize].clone();
+        let mpa_next=next_interpolated[Indices::MemoryPermuation as usize].clone();
+        let iea_next=next_interpolated[Indices::InputEvaluation as usize].clone();
+        let oea_next=next_interpolated[Indices::OutputEvaluation as usize].clone();
+        let one=Polynomial::new_from_coefficients(vec![FieldElement::one(self.table.field)]);
+        let mut AIR=vec![];
+        //Boundary contsraints :clk=mp=mv=inv=ip=0
+        //iea=oea=1 (we are using 1 instead of any random number)
+        let poly_two=Polynomial::new_from_coefficients(vec![FieldElement::new(2,self.table.field)]);
+        let boundaryAIR=clk.clone()+ip.clone()+mp.clone()+mv.clone()+inv_mv.clone()+ipa.clone()+mpa.clone()+iea.clone()+oea.clone()-poly_two.clone();
+        AIR.push(boundaryAIR);
+        // Transition_i0,
+        // Transition_i1,
+        // Transition_i2,
+        // Transition_i3,
+        // Transition_i4,
+        // Transition_i5,
+        // Transition_i6,
+        // Transition_i7,
+        // Transition_all,
+        let poly_one=Polynomial::new_from_coefficients(vec![FieldElement::one(self.table.field)]);
+        let mv_is_zero=poly_one.clone()-mv.clone()*inv_mv.clone();
+        //(ip⋆−ip−2)⋅mv+(ip⋆−ni)⋅iszero
+        // mp⋆−mp
+        // mv⋆−mv
+        let trasition_i0=(mv.clone()*(ip_next.clone()-ip.clone()-poly_two.clone())+mv_is_zero.clone()*(ip_next.clone()-ni.clone()))+(mp_next.clone()-mp.clone())+(mv_next.clone()-mv.clone());
+        AIR.push(trasition_i0);
+        // (ip⋆−ip−2)⋅iszero+(ip⋆−ni)⋅mv
+        // mp⋆−mp
+        // mv⋆−mv
     
 
-            let trasition_i1=mv_is_zero.clone()*(ip_next.clone()-ip.clone()-poly_two.clone())+(ip_next.clone()-ni.clone())*mv.clone()+(mv_next.clone()-mv.clone())+(mp_next.clone()-mp.clone());
-            AIR.push(trasition_i1);
+        let trasition_i1=mv_is_zero.clone()*(ip_next.clone()-ip.clone()-poly_two.clone())+(ip_next.clone()-ni.clone())*mv.clone()+(mv_next.clone()-mv.clone())+(mp_next.clone()-mp.clone());
+        AIR.push(trasition_i1);
 
-            // ip⋆−ip−1
-            // mp⋆−mp+1
-            let trasition_i2=(ip_next.clone()-ip.clone()-poly_one.clone())+(mp_next.clone()-mp.clone()+poly_one.clone());
-            AIR.push(trasition_i2);
-            // ip⋆−ip−1
-            // mp⋆−mp+1
-            let trasition_i3=(ip_next.clone()-ip.clone()-poly_one.clone())+
-            (mp_next.clone()-mp.clone()+poly_one.clone());
-                AIR.push(trasition_i3);
+        // ip⋆−ip−1
+        // mp⋆−mp+1
+        let trasition_i2=(ip_next.clone()-ip.clone()-poly_one.clone())+(mp_next.clone()-mp.clone()+poly_one.clone());
+        AIR.push(trasition_i2);
+        // ip⋆−ip−1
+        // mp⋆−mp+1
+        let trasition_i3=(ip_next.clone()-ip.clone()-poly_one.clone())+(mp_next.clone()-mp.clone()+poly_one.clone());
+        AIR.push(trasition_i3);
 
-    //ip⋆−ip−1
-    // mp⋆−mp
-    // mv⋆−mv−1
-     let trasition_i4=(ip_next.clone()-ip.clone()-poly_one.clone())+
-     (mp_next.clone()-mp.clone())+
-     (mv_next.clone()-mv.clone()-poly_one.clone());
+        //ip⋆−ip−1
+        // mp⋆−mp
+        // mv⋆−mv−1
+        let trasition_i4=(ip_next.clone()-ip.clone()-poly_one.clone())+
+        (mp_next.clone()-mp.clone())+
+        (mv_next.clone()-mv.clone()-poly_one.clone());
         AIR.push(trasition_i4);
-    // ip⋆−ip−1
-    // mp⋆−mp
-    // mv⋆−mv+1
-    let trasition_i5=(ip_next.clone()-ip.clone()-poly_one.clone())+
-    (mp_next.clone()-mp.clone())+
-    (mv_next.clone()-mv.clone()-poly_one.clone());
-    AIR.push(trasition_i5);
-    //  ip⋆−ip−1
-    // mp⋆−mp
+        // ip⋆−ip−1
+        // mp⋆−mp
+        // mv⋆−mv+1
+        let trasition_i5=(ip_next.clone()-ip.clone()-poly_one.clone())+
+        (mp_next.clone()-mp.clone())+
+        (mv_next.clone()-mv.clone()-poly_one.clone());
+        AIR.push(trasition_i5);
+        //  ip⋆−ip−1
+        // mp⋆−mp
 
-            let trasition_i6=(ip_next.clone()-ip.clone()-poly_one.clone())+
-            (mp_next.clone()-mp.clone());
-            AIR.push(trasition_i6);
-            //  ip⋆−ip−1
-            // mp⋆−mp
-            // mv⋆−mv
-            let trasition_i7=(ip_next.clone()-ip.clone()-poly_one.clone())+(mp_next.clone()-mp.clone())+
-            (mv_next.clone()-mv.clone());
-            AIR.push(trasition_i7);
-            //clk⋆−clk−1
-            // inv⋅(1−inv⋅mv) 
-            //ci.(ipa.(a.ip+b.ci+c.ni-alpha)-ipa*)+(ipa*-ipa).deselector
-            //mpa.(d.clk+e.mp+f.mv-beta)-mpa*
-            //selector(,)(ci) . (iea.gamma + mv - iea*) + (ci -  “,”) . (iea - iea*)
-            //selector(.)(ci) . (oea.delta + mv - oea*) + (ci -  “.”) . (oea - oea*)
+        let trasition_i6=(ip_next.clone()-ip.clone()-poly_one.clone())+
+        (mp_next.clone()-mp.clone());
+        AIR.push(trasition_i6);
+        //  ip⋆−ip−1
+        // mp⋆−mp
+        // mv⋆−mv
+        let trasition_i7=(ip_next.clone()-ip.clone()-poly_one.clone())+(mp_next.clone()-mp.clone())+
+        (mv_next.clone()-mv.clone());
+        AIR.push(trasition_i7);
+        //clk⋆−clk−1
+        // inv⋅(1−inv⋅mv) 
+        //ci.(ipa.(a.ip+b.ci+c.ni-alpha)-ipa*)+(ipa*-ipa).deselector
+        //mpa.(d.clk+e.mp+f.mv-beta)-mpa*
+        //selector(,)(ci) . (iea.gamma + mv - iea*) + (ci -  “,”) . (iea - iea*)
+        //selector(.)(ci) . (oea.delta + mv - oea*) + (ci -  “.”) . (oea - oea*)
             
-            // @todo
-            let trasition_all=(clk_next.clone()-clk.clone()-poly_one.clone())+
-            (inv_mv.clone()*(mv_is_zero.clone()));
+        // @todo
+        let trasition_all=(clk_next.clone()-clk.clone()-poly_one.clone())+
+        (inv_mv.clone()*(mv_is_zero.clone()));
 
-            AIR.push(trasition_all);       
-            AIR   
+        AIR.push(trasition_all);       
+        AIR   
     }
 }
 // @todo test processor table padding
